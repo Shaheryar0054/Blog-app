ce35227b84c910fd47ab50eaf9b06db61fb6519c
shaheryar abid <sharyar0310@gmail.com>
2023-08-19 21:51:11 +0500

    Added validation and modal specs

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,9 @@
 class Like < ApplicationRecord
-  belongs_to :user
+  belongs_to :author, class_name: 'User'
   belongs_to :post
+
+  validates :author_id, presence: true
+  validates :post_id, presence: true
 
   after_save :update_counter
 
